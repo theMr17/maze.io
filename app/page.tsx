b8d4dd899b1f3459fed9860ed84662f3c2555b8b
Saptak Manna <saptakmanna100@gmail.com>
2025-07-08 19:31:59 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,60 @@
 import Image from "next/image";
 
 export default function Home() {
+  const username = "Player1";
+  const playerLevel = 53;
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="maze-pattern-bg h-screen flex items-center justify-center">
+      <div className="absolute top-4 left-4 flex items-center">
+        <div className="player-level-bg size-18 flex items-center justify-center z-10">
+          <span className="text-xl text-black">{playerLevel}</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="-translate-x-9 bg-tertiary border-6 border-border pl-10 pr-4 py-1">
+          <span className="text-2xl">{username}</span>
+        </div>
+      </div>
+
+      <div className="absolute top-4 right-4 flex space-x-4">
+        <div className="relative size-10 cursor-pointer">
+          <Image src="/settings.svg" alt="Settings" fill />
+        </div>
+        <div className="relative size-10 cursor-pointer">
+          <Image src="/volume.svg" alt="Volume" fill />
+        </div>
+      </div>
+
+      <div className="flex h-full w-full">
+        <div className="flex flex-col justify-center w-64">
+          <div className="bg-tertiary p-4 align-center justify-center">
+            <h2 className="text-white mb-2">Customize</h2>
+            <input
+              placeholder="UserName"
+              className="w-full p-2 mb-2 text-black rounded"
+            />
+            <button className="bg-cyan-300 text-black w-full py-2 rounded mb-2">
+              Save
+            </button>
+            <button className="bg-yellow-200 text-black w-full py-2 rounded">
+              Sign Out
+            </button>
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col justify-center items-center">
+          <img src="maze-logo.svg" alt="Maze Logo" className="w-70" />
+          <button className=" bg-secondary text-white text-3xl w-full max-w-sm px-6 py-2 border-border border-6 mt-10 cursor-pointer">
+            Play
+          </button>
+          <div className="flex space-x-4 w-full max-w-sm mt-5">
+            <button className="flex-1 bg-tertiary text-white text-2xl px-6 py-2 cursor-pointer">
+              Create
+            </button>
+            <button className="flex-1 bg-tertiary text-white text-2xl px-6 py-2 cursor-pointer">
+              Join
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
